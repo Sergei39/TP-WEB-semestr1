f391c43a8b16b6b37cfe1edf438eabc2a2e9db94
Sergei Shishkin <3shishkin39@mail.ru>
2020-11-03 17:33:58 +0300

    static + templating

--- askme/app/views.py
+++ askme/app/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html', {})
+
+
+def ask(request):
+    return render(request, 'ask.html', {})
+
+
+def login(request):
+    return render(request, 'login.html', {})
+
+
+def question(request):
+    return render(request, 'question.html', {})
+
+
+def settings(request):
+    return render(request, 'settings.html', {})
+
+
+def signup(request):
+    return render(request, 'signup.html', {})
+
+
+def tag(request):
+    return render(request, 'tag.html', {})
--- askme/askme/urls.py
+++ askme/askme/urls.py
@@ -16,6 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
+    path('ask/', views.ask),
+    path('login/', views.login),
+    path('question/', views.question),
+    path('settings/', views.settings),
+    path('signup/', views.signup),
+    path('tag/', views.tag),
 ]
