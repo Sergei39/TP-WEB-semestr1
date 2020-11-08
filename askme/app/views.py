74b3add57afa86b0d1b1bc2f7a641ef762fca730
Sergei Shishkin <3shishkin39@mail.ru>
2020-11-08 16:19:49 +0300

    add pagination and correction

--- askme/app/views.py
+++ askme/app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+import random
+import itertools
+
 
 # Create your views here.
 
@@ -6,20 +10,41 @@ questions = [
     {
         'id': idx,
         'title': f'My best question {idx}',
+        'like': random.randint(-5, 5),
+        'answer': random.randint(0, 5),
         'text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur... ',
-    } for idx in range(10)
+    } for idx in range(20)
 ]
+
+answers = [
+    {
+        'id': idx,
+        'title': f'answer {idx}',
+        'like': random.randint(-5, 5),
+        'text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat... ',
+    } for idx in range(7)
+]
+
+def paginate(request, object_list, per_page=7):
+    paginator = Paginator(object_list, per_page)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return page_obj
 
 
 def index(request):
-    return render(request, 'index.html', {
-        'questions': questions,
+    page_obj = paginate(request, questions)
+    return render(request,'index.html', {
+        'questions':page_obj,
+        'page_obj': page_obj
     })
 
 
 def hot(request):
-    return render(request, 'hot.html', {
-        'questions': questions,
+    page_obj = paginate(request, questions)
+    return render(request,'hot.html', {
+        'questions':page_obj,
+        'page_obj': page_obj
     })
 
 
@@ -33,8 +58,12 @@ def login(request):
 
 def question(request, pk):
     question = questions[pk]
+    this_answers = answers[:question.get('answer')]
+    page_obj = paginate(request, this_answers)
     return render(request, 'question.html', {
         'question': question,
+        'answers': page_obj,
+        'page_obj': page_obj,
     })
 
 
@@ -47,7 +76,9 @@ def signup(request):
 
 
 def tag(request, tagname):
+    page_obj = paginate(request, questions)
     return render(request, 'tag.html', {
-        'questions': questions,
+        'questions':page_obj,
+        'page_obj': page_obj,
         'tagname': tagname,
     })
