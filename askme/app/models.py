457f6da304d810facedb8867ad6fb0faa67a427a
Sergei Shishkin <3shishkin39@mail.ru>
2020-11-17 17:26:18 +0300

    postgreSQL implemented

--- askme/app/models.py
+++ askme/app/models.py
@@ -28,7 +28,7 @@ class User(models.Model):
 class Profile(models.Model):
     nick_name = models.CharField(max_length=256, verbose_name='Имя')
     birthday = models.DateField(verbose_name='Дата рождения', blank=True, null=True)
-    avatar = models.ImageField(upload_to='static/madia/image/avatar/',
+    avatar = models.ImageField(upload_to='static/media/image/avatar/',
                                 default = 'static/media/image/avatar/200.jpeg',
                                 blank = True,
                                 verbose_name='Аватарка')
